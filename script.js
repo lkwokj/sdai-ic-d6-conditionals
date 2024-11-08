27afd47a7d008b5f6803f49098b102f90d909a46
Lily Kwok <lkwokj@gmail.com>
2024-11-08 05:14:01 +0000

    added instructions

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // all code for this lab to be completed in this file
+let currentTemperature = 30
+console.log("currentTemperature", currentTemperature)
+
+//there are two possibilities:  it's freezing or it's not
+if (currentTemperature <= 32) { 
+    console.log('It is freezing.BRRRR')
+} else {
+    console.log('It is not freezing, thank goodness')
+}
+
+//challenge #2
+let age1 = 50
+let age2 = "50"
+
+//3 possibilities:  Older, younger or the same age
+//if... else if statement
+
+if (age1 > age2) {//older
+    console.log("Respect!")
+} else if (age1 < age2) {//younger
+    console.log("whippersnapper")
+} else if (age1 === age2) {//same age, triple equals more strict
+    console.log("Cool!")
+} else { //capture errors - pro level
+    console.log("Error, try again!")
+}
